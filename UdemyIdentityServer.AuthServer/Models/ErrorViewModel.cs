c6e329cc7794ca75db632890757f7a9adaca3e31
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-09-23 21:34:20 +0300

    Add project files.

---
+++ UdemyIdentityServer.AuthServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UdemyIdentityServer.AuthServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
