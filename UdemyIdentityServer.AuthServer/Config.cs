1d54b404d769ad2f2f4ab5e6429d652ef57a12b7
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-09-23 23:13:00 +0300

    Client credentials grant  proje uygulaması-1

---
+++ UdemyIdentityServer.AuthServer/Config.cs
@@ -0,0 +1,37 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemyIdentityServer.AuthServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("resource_api1"){
+                    Scopes={ "api1.read","api1.write","api1.update" } },
+                new ApiResource("resource_api2")
+                {
+                       Scopes={ "api2.read","api2.write","api2.update" }
+                }
+            };
+        }
+
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>()
+            {
+                new ApiScope("api1.read","API 1 için okuma izni"),
+                  new ApiScope("api1.write","API 1 için yazma izni"),
+                    new ApiScope("api1.update","API 1 için güncelleme izni"),
+                       new ApiScope("api2.read","API 2 için okuma izni"),
+                  new ApiScope("api2.write","API 2 için yazma izni"),
+                    new ApiScope("api2.update","API 2 için güncelleme izni"),
+            };
+        }
+    }
+}
