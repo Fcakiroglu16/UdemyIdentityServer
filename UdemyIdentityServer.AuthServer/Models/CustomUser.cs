b932d8aa5c846dd2c45d22bf76f2b3365e8ebd78
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-10-04 14:36:16 +0300

    Custom Üyelik Sistemi-1(Merkezi)

---
+++ UdemyIdentityServer.AuthServer/Models/CustomUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemyIdentityServer.AuthServer.Models
+{
+    public class CustomUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+    }
+}
