92e0d92be048fef160dc2f4778a863514fc8752e
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-10-05 22:15:38 +0300

    Resource Owner Credentials Grant-3

--- UdemyIdentityServer.AuthServer/Config.cs
+++ UdemyIdentityServer.AuthServer/Config.cs
@@ -142,7 +142,22 @@ namespace UdemyIdentityServer.AuthServer
                       RedirectUris={"http://localhost:4200/callback"},
                       AllowedCorsOrigins={"http://localhost:4200"},
                       PostLogoutRedirectUris={"http://localhost:4200" }
-                  }
+                  },
+
+                          new Client()
+                 {
+                     ClientId = "Client1-ResourceOwner-Mvc",
+
+                    ClientName="Client 1 app  mvc uygulaması",
+                   ClientSecrets=new[] {new Secret("secret".Sha256())},
+                   AllowedGrantTypes= GrantTypes.ResourceOwnerPassword,
+                   AllowedScopes = { IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile, "api1.read",IdentityServerConstants.StandardScopes.OfflineAccess,"CountryAndCity","Roles"},
+                   AccessTokenLifetime=2*60*60,
+                   AllowOfflineAccess=true,
+                   RefreshTokenUsage=TokenUsage.ReUse,
+                   RefreshTokenExpiration=TokenExpiration.Absolute,
+                   AbsoluteRefreshTokenLifetime=(int) (DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
+        },
     };
         }
     }
