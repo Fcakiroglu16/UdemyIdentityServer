60efd2f9f117eae47f056c3c13f3a4c950b95b7c
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-10-11 21:37:46 +0300

    IdentityServer4  ASP.NET Core Identity -4

--- UdemyIdentityServer-IdentityAPI.AuthServer/Controllers/UserController.cs
+++ UdemyIdentityServer-IdentityAPI.AuthServer/Controllers/UserController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using UdemyIdentityServer_IdentityAPI.AuthServer.Models;
+using UdemyIdentityServerIdentityAPI.AuthServer.Models;
 using static IdentityServer4.IdentityServerConstants;
 
 namespace UdemyIdentityServerIdentityAPI.AuthServer.Controllers
@@ -17,10 +18,30 @@ namespace UdemyIdentityServerIdentityAPI.AuthServer.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult SignUp()
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
         {
-            return Ok("signup çalıştı");
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserSaveViewModel userSaveViewModel)
+        {
+            ApplicationUser applicationUser = new ApplicationUser();
+
+            applicationUser.UserName = userSaveViewModel.UserName;
+            applicationUser.Email = userSaveViewModel.Email;
+            applicationUser.City = userSaveViewModel.City;
+
+            var result = await _userManager.CreateAsync(applicationUser, userSaveViewModel.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors.Select(x => x.Description));
+            }
+
+            return Ok("üye başarıyla kaydedildi");
         }
     }
 }
