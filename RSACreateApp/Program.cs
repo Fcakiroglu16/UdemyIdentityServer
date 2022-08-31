a866664f4935fcae6d658f0583e5f6f10268346d
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2022-09-01 01:47:36 +0300

    Signin Credential :RSA

---
+++ RSACreateApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+
+Console.WriteLine("Hello, World!");
+
+using var rsa = RSA.Create();
+Console.WriteLine(
+    $"-----Private key-----{Environment.NewLine}{Convert.ToBase64String(rsa.ExportRSAPrivateKey())}{Environment.NewLine}");
+Console.WriteLine($"-----Public key-----{Environment.NewLine}{Convert.ToBase64String(rsa.ExportRSAPublicKey())}");
--- UdemyIdentityServer.AuthServer/Startup.cs
+++ UdemyIdentityServer.AuthServer/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace UdemyIdentityServer.AuthServer
 {
@@ -23,12 +25,29 @@ namespace UdemyIdentityServer.AuthServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIdentityServer()
-                .AddInMemoryApiResources(Config.GetApiResources())
-                .AddInMemoryApiScopes(Config.GetApiScopes())
-                .AddInMemoryClients(Config.GetClients())
-                .AddDeveloperSigningCredential();
-                
+          var  rsa = RSA.Create();
+          
+              rsa.ImportRSAPrivateKey(
+              Convert.FromBase64String(Configuration["Jwt:Asymmetric:PrivateKey"]),
+              out _);
+
+              rsa.ImportRSAPublicKey(
+                  Convert.FromBase64String(Configuration["Jwt:Asymmetric:PublicKey"]),
+                  out _
+              );
+
+
+
+
+              services.AddIdentityServer()
+                  .AddInMemoryApiResources(Config.GetApiResources())
+                  .AddInMemoryApiScopes(Config.GetApiScopes())
+                  .AddInMemoryClients(Config.GetClients())
+                  .AddSigningCredential(new SigningCredentials(
+                      new RsaSecurityKey(rsa),
+                      SecurityAlgorithms.RsaSha256 // Important to use RSA version of the SHA algo 
+                  ));
+                  
 
             services.AddControllersWithViews();
         }
